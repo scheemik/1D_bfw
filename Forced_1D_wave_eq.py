d9ad7b5004c6baa1e52074d1663802e239bb19cd
Mikhail <mschee@physics.utoronto.ca>
2020-03-16 10:52:04 -0400

    Oriented t on horizontal and z on vertical axes

--- Forced_1D_wave_eq.py
+++ Forced_1D_wave_eq.py
@@ -1,7 +1,7 @@
 """
 1D Forced wave equation:
 
-d_{tt}u + a*d_{xx}u = F
+d_{tt}w + a*d_{zz}w = F
 
 This script should be ran serially (because it is 1D), and creates a space-time
 plot of the computed solution.
@@ -20,34 +20,34 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Bases and domain
-x_basis = de.Fourier('x', 1024, interval=(-2, 16))#, dealias=3/2)
+z_basis = de.Fourier('z', 1024, interval=(-2, 16))#, dealias=3/2)
 # something weird happens when I use dealias, the number of grid points in x don't line up anymore. I put in 1024 but get back 1536
-domain = de.Domain([x_basis], np.float64)
+domain = de.Domain([z_basis], np.float64)
 
 # Problem parameters
 a=1.
 
-#X grid
-#x = domain.grid(0, scales=domain.dealias)
-x = domain.grid(0)
+#Z grid
+#z = domain.grid(0, scales=domain.dealias)
+z = domain.grid(0)
 
 #define forcing function
-def forcing(x,solver):
-     return np.cos(x-solver.sim_time);
+def forcing(z,solver):
+     return np.cos(z-solver.sim_time);
 #     return solver.sim_time;
-#     return np.cos(x);
+#     return np.cos(z);
 
 #assign GeneralFunction to parameter 'F'
 F= GeneralFunction(domain,'g',forcing,args=[])
 
 # Problem
-problem = de.IVP(domain, variables=['u', 's', 'r'])
+problem = de.IVP(domain, variables=['w', 'wt', 'wz'])
 
 problem.parameters['a'] = a
 problem.parameters['F'] = F
-problem.add_equation("dt(s) + a*dx(r)= F")
-problem.add_equation("s - dt(u) = 0")
-problem.add_equation("r + a*dx(u) = 0")
+problem.add_equation("dt(wt) + a*dz(wz)= F")
+problem.add_equation("wt - dt(w) = 0")
+problem.add_equation("wz + a*dz(w) = 0")
 
 # Build solver
 solver = problem.build_solver(de.timesteppers.SBDF2)
@@ -55,26 +55,26 @@ solver.stop_wall_time = 10000
 solver.stop_iteration = 10000
 
 #pass the relevant arguments to the forcing function
-F.args = [x,solver]
-F.original_args = [x,solver]
+F.args = [z,solver]
+F.original_args = [z,solver]
 
 # Above code modified from here: https://groups.google.com/forum/#!searchin/dedalus-users/%22wave$20equation%22%7Csort:date/dedalus-users/TJEOwHEDghU/g2x00YGaAwAJ
 
 ###############################################################################
 
 # Initial conditions
-u = solver.state['u']
-s = solver.state['s']
-r = solver.state['r']
+w  = solver.state['w']
+wt = solver.state['wt']
+wz = solver.state['wz']
 
-u['g'] = 0.0
-u.differentiate('x', out=r)
-s['g'] = 0.0
-#u.differentiate('t', out=s)
+w['g'] = 0.0
+w.differentiate('z', out=wz)
+wt['g'] = 0.0
+#w.differentiate('t', out=wt)
 
 # Store data for final plot
-u.set_scales(1)
-u_list = [np.copy(u['g'])]
+w.set_scales(1)
+w_list = [np.copy(w['g'])]
 t_list = [solver.sim_time]
 
 # Main loop
@@ -85,8 +85,8 @@ try:
     while solver.proceed:
         solver.step(dt)
         if solver.iteration % 20 == 0:
-            u.set_scales(1)
-            u_list.append(np.copy(u['g']))
+            w.set_scales(1)
+            w_list.append(np.copy(w['g']))
             t_list.append(solver.sim_time)
         if solver.iteration % 100 == 0:
             logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
@@ -101,17 +101,17 @@ finally:
     logger.info('Run time: %f cpu-hr' %((end_time-start_time)/60/60*domain.dist.comm_cart.size))
 
 # Create space-time plot
-u_array = np.array(u_list)
-print('shape of u:',u_array.shape)
+w_array = np.transpose(np.array(w_list))
+print('shape of w:', w_array.shape)
 t_array = np.array(t_list)
 print('shape of t:',t_array.shape)
-print('shape of x:',x.shape)
-xmesh, ymesh = quad_mesh(x=x, y=t_array)
+print('shape of z:',z.shape)
+xmesh, ymesh = quad_mesh(x=t_array, y=z)
 plt.figure()
-plt.pcolormesh(xmesh, ymesh, u_array, cmap='RdBu_r')
+plt.pcolormesh(xmesh, ymesh, w_array, cmap='RdBu_r')
 plt.axis(pad_limits(xmesh, ymesh))
 plt.colorbar()
-plt.xlabel('x')
-plt.ylabel('t')
+plt.xlabel('t')
+plt.ylabel('z')
 plt.title('Forced 1D Wave, (a,F)=(%g,%g)' %(problem.parameters['a'], problem.parameters['a']))
 plt.savefig('f_1D_wave.png')
